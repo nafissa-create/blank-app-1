267bd2e80d75579798e4a4e335bb5809af42ecb6
nafissa-create <Wendpouire.Ouedraogo@strathmore.edu>
2025-06-30 21:17:44 +0300

    Update predictor_model.py

--- predictor_model.py
+++ predictor_model.py
@@ -5,8 +5,4 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pd.read_csv("diamonds.csv")
-print(df.head())
-print(df.info())
-print(df.describe())
-print(df.isnull().sum())
+
